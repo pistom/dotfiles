051c730ff7a7669516341256e18406a0a7835d6d
Tomasz <pistom@users.noreply.github.com>
2024-11-11 13:53:45 +0100

    Update monitors.sh

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -7,7 +7,7 @@ CONNECTED_MONITORS=$(xrandr --listmonitors | grep '^[[:space:]]*[0-9]:' )
 DELL_HOME_MONITOR="DP-2 3840/880x1600/370"
 DELL_WORK_MONITOR="DP-2 2560/597x1440/336"
 
-echo $HOME_MONITOR
+# Use tools like arandr to generate andr output command
 if echo "$CONNECTED_MONITORS" | grep -q "$DELL_HOME_MONITOR"; then
     xrandr --output eDP-1 --primary --mode 1680x1050 --pos 0x1243 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 3840x1600 --pos 1680x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
 elif echo "$CONNECTED_MONITORS" | grep -q "$DELL_WORK_MONITOR"; then
