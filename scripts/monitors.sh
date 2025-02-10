8b3c1388c469d9c6a0ba4c9c6f544fbd4d835389
Tomasz <tomasz.pisarek@gmail.com>
2025-02-10 13:26:28 +0100

    add dunst dracula, fix msi display settings

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -6,7 +6,7 @@ CONNECTED_MONITORS=$(xrandr --listmonitors | grep '^[[:space:]]*[0-9]:' )
 # Define your monitor setups
 DELL_HOME_MONITOR="DP-2 3840/880x1600/370"
 DELL_WORK_MONITOR="DP-2 2560/597x1440/336"
-MSI_HOME_MONITOR="HDMI-0 3840/879x1600/366"
+MSI_HOME_MONITOR="HDMI-1-0 3840/879x1600/366"
 
 # Use tools like arandr to generate andr output command
 if echo "$CONNECTED_MONITORS" | grep -q "$DELL_HOME_MONITOR"; then
@@ -14,7 +14,7 @@ if echo "$CONNECTED_MONITORS" | grep -q "$DELL_HOME_MONITOR"; then
 elif echo "$CONNECTED_MONITORS" | grep -q "$DELL_WORK_MONITOR"; then
     xrandr --output eDP-1 --primary --mode 1680x1050 --pos 2560x165 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
 elif echo "$CONNECTED_MONITORS" | grep -q "$MSI_HOME_MONITOR"; then
-    xrandr --output HDMI-0 --primary --mode 3840x1600 --pos 1920x128 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-0 --mode 3840x1600 --pos 1920x660 --rotate normal
 else
     echo "Unknown monitor setup:"
     echo "$CONNECTED_MONITORS"
